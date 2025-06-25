7cd7c7964793615e2fce1833cc9d463d8a6c7988
Chinmay-Jadhav-04 <chinmayrjadhav4122003@gmail.com>
2025-06-25 18:45:36 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './sections/Navbar'
+import Hero from './sections/Hero'
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <section classname="min-h-screen"/>
+      <section classname="min-h-screen"/>
+      <section classname="min-h-screen"/>
+      <section classname="min-h-screen"/>
+      <section classname="min-h-screen"/>
+      <section classname="min-h-screen"/>
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testomonials */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  )
+}
+
+export default App
