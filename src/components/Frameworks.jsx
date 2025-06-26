6e81d4de9cd161c470075f97cc1c33e876e69f0f
Chinmay-Jadhav-04 <chinmayrjadhav4122003@gmail.com>
2025-06-26 20:49:30 +0530

    About Me Section Completed

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -1,68 +1,40 @@
 import { OrbitingCircles } from "./OrbitingCircles";
 
-
 export function Frameworks() {
-
-    const skills = [
-        "c",
-        "cplusplus",
-        "css3",
-        "tailwindcss",
-        "html5",
-        "java",
-        "eclipse",
-        "javascript",
-        "microsoft",
-        "python",
-        "linux",
-        "react",
-        "typescript",
-        "netlify",
-        "vercel",
-        "vite.js",
-        "next.js",
-        "three.js",
-        "wordpress",
-        "visualstudiocode",
-        "pocketbase",
-        "mongodb",
-        "mysql",
-        "postman",
-        "git",
-        "github",
-        "arduino",
-        "raspberrypie",
-    ]
-
+  const skills = [
+    "auth0",
+    "blazor",
+    "cplusplus",
+    "csharp",
+    "css3",
+    "dotnet",
+    "dotnetcore",
+    "git",
+    "html5",
+    "javascript",
+    "microsoft",
+    "react",
+    "sqlite",
+    "tailwindcss",
+    "vitejs",
+    "wordpress",
+  ];
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
-
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
-            <Icon 
-            key={index}
-            src={`assets/logos/${skill}.svg`}
-            />
-            )
-            )}
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
       </OrbitingCircles>
-      <OrbitingCircles 
-      iconSize={25} 
-      radius={100} 
-      reverse speed={2}
-      >
-       {skills.reverse().map((skill, index) => (
-            <Icon 
-            key={index}
-            src={`assets/logos/${skill}.svg`}
-            />
-            )
-            )}
+      <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
+        {skills.reverse().map((skill, index) => (
+          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+        ))}
       </OrbitingCircles>
     </div>
   );
 }
 
-const Icon = ({src}) => {
-    <img src={src} className="duration-200 rounded-sm hover:scale-110"/>
-}
+const Icon = ({ src }) => (
+  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+);
